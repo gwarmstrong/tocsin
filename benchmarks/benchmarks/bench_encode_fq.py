ecd7602b603727f9e355ad691af5862f75411339
George Armstrong <garmstrong@colgate.edu>
2020-03-10 11:03:15 -0700

    update cython bindings for int8

--- benchmarks/benchmarks/bench_encode_fq.py
+++ benchmarks/benchmarks/bench_encode_fq.py
@@ -4,9 +4,8 @@ import logging
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # FATAL
 import pkg_resources
 from tocsin.utils import dna_table
-from tocsin.reader import fq_reader, fq_encoder
+from tocsin.reader import fq_reader, fq_encoder, fq_encoder_alt
 import tensorflow as tf
-import numpy as np
 
 logging.getLogger('tensorflow').setLevel(logging.ERROR)
 
@@ -27,6 +26,9 @@ class TimeReadingSuite:
     def time_read_fq_encoder_cpp(self):
         fq_encoder(bytes(self.abs_path, 'utf-8'))
 
+    def time_cython_encoder_alt(self):
+        lines = fq_encoder_alt(bytes(self.abs_path, 'utf-8'))
+
 
 class TimeEncodingSuite:
     """
@@ -40,13 +42,14 @@ class TimeEncodingSuite:
         with gzip.open(self.abs_path) as fp:
             self.lines = fp.readlines()
 
+        self.safe_max_read_length = 100
         self.max_read_length = 100
 
     def time_indexed_list_python_tf(self):
         lines = []
         for i, line in enumerate(self.lines):
             if i % 4 == 1:
-                bytes_ = line.rstrip()[:self.max_read_length]
+                bytes_ = line.rstrip()[:self.safe_max_read_length]
                 lines.append([byte_ for byte_ in bytes_])
 
         ord_list = dna_table.lookup(tf.constant(lines))
@@ -55,25 +58,49 @@ class TimeEncodingSuite:
         lines = []
         for i, line in enumerate(self.lines):
             if i % 4 == 1:
-                bytes_ = line.rstrip()[:self.max_read_length]
+                bytes_ = line.rstrip()[:self.safe_max_read_length]
                 lines.append([byte_ for byte_ in bytes_])
         ord_list = dna_table.lookup(tf.constant(lines))
         encoding = tf.one_hot(ord_list, 4)
 
 
-    def time_python_end_to_end(self):
+    def time_python_end_to_end_v1(self):
         with gzip.open(self.abs_path) as fp:
             all_lines = fp.readlines()
 
+        length = self.max_read_length
+        lines = []
+        for i, line in enumerate(all_lines):
+            if i % 4 == 1:
+                bytes_ = line.rstrip()# [:self.max_read_length]
+                bytes_ = [byte_ for byte_ in bytes_]
+                diff = length - len(bytes_)
+                if diff > 0:
+                    bytes_ += (diff * [-1])
+                elif diff < 0:
+                    bytes_ = bytes_[:length]
+                lines.append(bytes_)
+
+        ord_list = dna_table.lookup(tf.constant(lines))
+        encoding = tf.one_hot(ord_list, 4)
+
+    def time_python_end_to_end_v2(self):
+        with gzip.open(self.abs_path) as fp:
+            all_lines = fp.readlines()
+
+        length = self.max_read_length
         lines = []
         for i, line in enumerate(all_lines):
             if i % 4 == 1:
                 bytes_ = line.rstrip()[:self.max_read_length]
-                lines.append([byte_ for byte_ in bytes_])
+                bytes_ = [byte_ for byte_ in bytes_]
+                lines.append(bytes_)
 
         ord_list = dna_table.lookup(tf.constant(lines))
         encoding = tf.one_hot(ord_list, 4)
 
     def time_cython_end_to_end(self):
-        lines = fq_encoder(bytes(self.abs_path, 'utf-8'), length=100)
-        encoding = tf.one_hot(np.array(lines), 4)
+        lines = fq_encoder(bytes(self.abs_path, 'utf-8'),
+                           length=self.max_read_length)
+        encoding = tf.one_hot(tf.constant(lines), 4)
+
--- tocsin/reader.pxd
+++ tocsin/reader.pxd
@@ -8,6 +8,6 @@ cdef extern from "<stdint.h>" nogil:
 
 cdef extern from "../fastp-bind/src/api.h":
     vector[string] fastq_reader(string fastq_filename)
-    vector[vector[int]] fastq_encoder(string fastq_filename, int length)
+    vector[vector[int8_t]] fastq_encoder(string fastq_filename, int length)
     vector[vector[int8_t]] fastq_encoder_alt(string fastq_filename,
                                               int length)
